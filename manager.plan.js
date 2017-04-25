3a8e031f1f7a854d42020c806ae089095a3d3e4e
Kian Kane <AmethystArchon@gmail.com>
2017-04-25 13:54:58 +0100

    Started on manager.plan

---
+++ manager.plan.js
@@ -0,0 +1,18 @@
+/* The plan manager
+ */
+ 
+module.exports =
+{
+	run: function(room)
+	{
+		// Find room elements
+		var creeps = room.find(FIND_MY_CREEPS);
+
+		var tickMap = [];
+		
+		for (var key in creeps)
+		{
+			
+		}
+	}
+};
