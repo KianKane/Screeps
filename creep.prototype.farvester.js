4224ba42765366827306dd3f23cc8a54ed259756
Kian Kane <AmethystArchon@gmail.com>
2017-04-29 14:19:25 +0100

    Fixed dangerous issue causing screeps to prioritize storage over spawns/extensions. (Screepocalypse)

--- creep.prototype.farvester.js
+++ creep.prototype.farvester.js
@@ -40,13 +40,12 @@ Creep.prototype.farvester = function()
         {
             if (this.room.name === this.memory.home)
             {
-                var structure = structure = this.pos.findClosestByPath(this.room.find(FIND_MY_STRUCTURES,
-                    {filter: function(structure){ return (structure.energy < structure.energyCapacity && structure.structureType !== STRUCTURE_CONTAINER && structure.structureType !== STRUCTURE_STORAGE) ||
-                        ((structure.structureType !== STRUCTURE_CONTAINER || structure.structureType !== STRUCTURE_STORAGE) && _.sum(structure.store) < structure.storeCapacity); }}));
+                var structure = this.pos.findClosestByPath(this.room.find(FIND_MY_STRUCTURES,
+                    {filter: function(structure){ return structure.energy < structure.energyCapacity; }}));
                 if (!structure)
                 {
                     structure = this.pos.findClosestByPath(this.room.find(FIND_MY_STRUCTURES,
-                        {filter: function(structure){ return structure.energy < structure.energyCapacity; }}));
+                        {filter: function(structure){ return (structure.structureType === STRUCTURE_CONTAINER || structure.structureType === STRUCTURE_STORAGE) && _.sum(structure.store) < structure.storeCapacity; }}));
                 }
                 if (structure)
                 {
